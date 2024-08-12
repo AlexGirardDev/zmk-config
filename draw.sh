6757e0f5f431265fdda79d027badcbca970abec8
Alex Girard <me@alexgirard.ca>
2024-08-12 17:33:50 -0400

    draw script

---
+++ draw.sh
@@ -0,0 +1,5 @@
+keymap parse -z config/corne.keymap > keymap/corne.yaml
+keymap draw keymap/corne.yaml > keymap/corne.svg
+
+# keymap -c keymap_drawer.config.yaml parse -z config/corne.keymap > keymap/corne.yaml
+# keymap -c keymap_drawer.config.yaml draw keymap/corne.yaml > keymap/corne.svg
